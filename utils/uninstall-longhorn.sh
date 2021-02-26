86784ade9c561d98aab889aa50010341f47d1b82
Roger Rumao <rumaor1@nationwide.com>
2021-02-26 09:27:34 -0600

    utils

---
+++ utils/uninstall-longhorn.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+kubectl create -f https://raw.githubusercontent.com/longhorn/longhorn/v1.1.0/uninstall/uninstall.yaml
+
+kubectl get job/longhorn-uninstall -w
+
+kubectl delete -f https://raw.githubusercontent.com/longhorn/longhorn/v1.1.0/deploy/longhorn.yaml
+kubectl delete -f https://raw.githubusercontent.com/longhorn/longhorn/v1.1.0/uninstall/uninstall.yaml
+
