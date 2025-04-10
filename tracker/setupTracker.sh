9844ca46f28db826c4c07666967f657377ebde14
Roger Rumao <rogerrum@gmail.com>
2025-04-10 10:32:18 -0500

    update

--- tracker/setupTracker.sh
+++ tracker/setupTracker.sh
@@ -5,6 +5,8 @@ GITHUB_URL="https://raw.githubusercontent.com/rogerrum/homelab-infrastructure/re
 TARGET_DIR="$HOME/.tracker"
 TARGET_FILE="$TARGET_DIR/metrics.sh"
 CRON_JOB="*/5 * * * * $TARGET_FILE"
+SELF_UPDATE_CRON_JOB="0 * * * * curl -sSL https://raw.githubusercontent.com/rogerrum/homelab-infrastructure/refs/heads/main/tracker/setupTracker.sh | bash"
+
 
 # Create the target directory if it doesn't exist
 mkdir -p "$TARGET_DIR"
@@ -17,3 +19,6 @@ chmod +x "$TARGET_FILE"
 
 # Add the cron job
 (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+
+# Add the cron job for self-update
+(crontab -l 2>/dev/null | grep -v "setupTracker.sh"; echo "$SELF_UPDATE_CRON_JOB") | crontab -
